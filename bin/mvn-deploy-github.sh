a6c2b1567aeebb39b27257d30d9c1031a0298ba8
Selamanse <selamanse@scheinfrei.info>
2022-02-25 16:44:19 +0100

    Add build dependency setup

---
+++ bin/mvn-deploy-github.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+MVN_SETTINGS_TEMPLATE_FILE=.mvn/settings.xml.tmpl
+MVN_SETTINGS_FILE=.mvn/settings.xml
+
+sed -e "s/GH_REPO/${GH_REPO}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
+sed -e "s/GH_DEPLOY_USERNAME/${GH_DEPLOY_USERNAME}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
+sed -e "s/GH_DEPLOY_TOKEN/${GH_DEPLOY_TOKEN}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
+
+mvn -s ${MVN_SETTINGS_FILE} --batch-mode deploy -Dlicense.skip=true
