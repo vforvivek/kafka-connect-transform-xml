540681741386246dd4739665f5c9212e9fdabedd
Selamanse <selamanse@scheinfrei.info>
2022-08-23 14:50:22 +0200

    Fix release prepare condition

--- bin/mvn-deploy-github.sh
+++ bin/mvn-deploy-github.sh
@@ -8,9 +8,7 @@ sed -e "s/GH_REPO/${GH_REPO}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_F
 sed -e "s/GH_DEPLOY_USERNAME/${GH_DEPLOY_USERNAME}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
 sed -e "s/GH_DEPLOY_TOKEN/${GH_DEPLOY_TOKEN}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
 
-if [ -z ${GITHUB_REF_NAME+x} ]; then
-  echo "no release tag. skipping release prepare...";
-else
+if [[ $GITHUB_REF_NAME =~ '^[0-9].*$' ]]; then
   echo "creating release version ${GITHUB_REF_NAME#v}"
   mvn -s ${MVN_SETTINGS_FILE} release:prepare -DreleaseVersion=${GITHUB_REF_NAME#v} || true
 fi
